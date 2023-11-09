fd421aa018577cd60c9ee2bd449af558f1662323
AlexisCepeda <alexisnatel@gmail.com>
2023-11-09 16:18:01 -0300

    update Usuario and added Rol

--- src/main/java/desarrollo/sprint4/apiresttest/Entity/Usuario.java
+++ src/main/java/desarrollo/sprint4/apiresttest/Entity/Usuario.java
@@ -1,5 +1,6 @@
 package desarrollo.sprint4.apiresttest.Entity;
 
+import desarrollo.sprint4.apiresttest.Enumeration.Rol;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -14,6 +15,18 @@ import java.time.LocalDate;
 @Builder
 public class Usuario extends BaseEntity{
 
+    @Column(name = "auth0id")
+    private String auth0Id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "rol")
+    private Rol rol;
+
     @Column(name = "fecha_alta_usuario")
     private LocalDate fechaAltaUsuario;
 
@@ -23,11 +36,6 @@ public class Usuario extends BaseEntity{
     @Column(name = "fecha_modificacion_usuario")
     private LocalDate fechaModificacionUsuario;
 
-    @Column(name = "username")
-    private String username;
-
-    @Column(name = "auth0id")
-    private String auth0Id;
 
     @OneToOne(cascade = CascadeType.PERSIST, orphanRemoval = true, fetch = FetchType.EAGER)
     @JoinColumn(name = "cliente_id")
---
+++ src/main/java/desarrollo/sprint4/apiresttest/Enumeration/Rol.java
@@ -0,0 +1,5 @@
+package desarrollo.sprint4.apiresttest.Enumeration;
+
+public enum Rol {
+    CLIENTE, CAJERO, COCINERO, DELIVERY, ADMIN
+}
